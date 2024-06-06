4a3bdc6def5ba3055f81365db2fd1afe198a3eb6
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-06-07 02:56:47 +0500

    Completed

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 
-
+const User = JSON.parse(localStorage.getItem('RegUser'));
+console.log(User);
+const Form = document.querySelector('form');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
+
+Form.addEventListener('submit' , (event)=>{
+    event.preventDefault();
+    const validation = User.find((human)=>{
+ return human.email === email.value && human.password === password.value;
+    })
+
+    if(!validation){
+        alert("Enter Correct Email & Password...!")
+        return;
+    }
+    window.location = "main.html"
+})
--- reg.js
+++ reg.js
@@ -3,27 +3,40 @@ const form =  document.querySelector('form');
 const fname = document.querySelector('#F-name');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
- 
 
-let User = [];
 
-form.addEventListener('submit', (event)=>{
-event.preventDefault();
+Form.addEventListener('submit', (event)=>{
 
-if (User.filter((items)=>{
-    return items.email === email.value;
-})) {
-    alert('!!!You have Already Registered!!!');
-}
-else{
-    User = {
-        fname: fname.value,
-        email: email.value,
-        password:password.value
+    event.preventDefault();
+    let RegisterUser;
+    let items = JSON.parse(localStorage.getItem('RegUser'));
+   if(items === null)
+    {
+        RegisterUser = [];
+    }
+    else{
+        RegisterUser = items;
+    }
+     const check = RegisterUser.find((user)=>{
+       return user.email === email.value;
+     });
+
+     if(check)
+        {
+            alert('Email Already Exists..!');
+            return;
         }
-}
 
+
+     NewUser = {
+        email: email.value,
+        password: password.value
+    }
+    RegisterUser.push(NewUser);
+    localStorage.setItem('RegUser', JSON.stringify(RegisterUser));
+
+    alert('Registration successful!');
+    Form.reset();
+
+    window.location = "index.html";
 })
-
-console.log(User);
-
