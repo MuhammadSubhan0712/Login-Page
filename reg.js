b6772392e67350f9423757236a51bdbf8131acaa
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-06-07 02:58:55 +0500

    Completed

--- reg.js
+++ reg.js
@@ -1,42 +1,35 @@
+const Form = document.querySelector("form");
+const fname = document.querySelector("#F-name");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
 
-const form =  document.querySelector('form');
-const fname = document.querySelector('#F-name');
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
+Form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let RegisterUser;
+  let items = JSON.parse(localStorage.getItem("RegUser"));
+  if (items === null) {
+    RegisterUser = [];
+  } else {
+    RegisterUser = items;
+  }
+  const check = RegisterUser.find((user) => {
+    return user.email === email.value;
+  });
 
+  if (check) {
+    alert("Email Already Exists..!");
+    return;
+  }
 
-Form.addEventListener('submit', (event)=>{
+  NewUser = {
+    email: email.value,
+    password: password.value,
+  };
+  RegisterUser.push(NewUser);
+  localStorage.setItem("RegUser", JSON.stringify(RegisterUser));
 
-    event.preventDefault();
-    let RegisterUser;
-    let items = JSON.parse(localStorage.getItem('RegUser'));
-   if(items === null)
-    {
-        RegisterUser = [];
-    }
-    else{
-        RegisterUser = items;
-    }
-     const check = RegisterUser.find((user)=>{
-       return user.email === email.value;
-     });
+  alert("Registration successful!");
+  Form.reset();
 
-     if(check)
-        {
-            alert('Email Already Exists..!');
-            return;
-        }
-
-
-     NewUser = {
-        email: email.value,
-        password: password.value
-    }
-    RegisterUser.push(NewUser);
-    localStorage.setItem('RegUser', JSON.stringify(RegisterUser));
-
-    alert('Registration successful!');
-    Form.reset();
-
-    window.location = "index.html";
-})
+  window.location = "index.html";
+});
